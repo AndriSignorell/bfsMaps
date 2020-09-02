ab494d8ec3e725b84065d0ec6d077c32e442d7b3
Andri Signorell <andri@signorell.net>
2020-09-02 19:22:10 +0200

    0.9.4.003

--- R/utils.R
+++ R/utils.R
@@ -107,6 +107,39 @@ CombineKant <- function(id, g, map=RequireMap("kant.map")){
 }
 
 
+RemoveHoles <- function (x) {
+
+  if (!any(which(utils::installed.packages()[, 1] %in% "maptools")))
+    stop("please install maptools package before running this function")
+
+  xp <- slot(x, "polygons")
+
+  holes <- lapply(xp, function(x) sapply(methods::slot(x, "Polygons"),
+                                         methods::slot, "hole"))
+  res <- lapply(1:length(xp),
+                function(i) methods::slot(xp[[i]], "Polygons")[!holes[[i]]])
+
+  IDs <- row.names(x)
+
+  x.fill <- sp::SpatialPolygons(lapply(1:length(res),
+                                       function(i) sp::Polygons(res[[i]],
+                                                  ID = IDs[i])), proj4string = sp::CRS(sp::proj4string(x)))
+  methods::slot(x.fill, "polygons") <- lapply(methods::slot(x.fill, "polygons"),
+                                              maptools::checkPolygonsHoles)
+  methods::slot(x.fill, "polygons") <- lapply(methods::slot(x.fill, "polygons"),
+                                              "comment<-", NULL)
+  pids <- sapply(methods::slot(x.fill, "polygons"),
+                 function(x) methods::slot(x,"ID"))
+
+  x.fill <- sp::SpatialPolygonsDataFrame(x.fill,
+                                         data.frame(row.names = pids, ID = 1:length(pids)))
+
+  return(x.fill)
+
+}
+
+
+
 
 # with(d.bfsrg,
 #      plot(
